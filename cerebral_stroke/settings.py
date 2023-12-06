3d5ab6420ca269f4b50e3ab83e4db9e0ba968c86
AyeshaNaime003 <ayeshashamim003@gmail.com>
2023-12-06 12:34:42 +0500

    templates setup

--- cerebral_stroke/settings.py
+++ cerebral_stroke/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # configure app(base) into the project
+    "base.apps.BaseConfig"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,9 @@ ROOT_URLCONF = "cerebral_stroke.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+             os.path.join(BASE_DIR,'cerebral_stroke','templates', ''),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
