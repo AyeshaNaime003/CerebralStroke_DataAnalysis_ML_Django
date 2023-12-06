f3e5f747350c8c693b9ace7e5f731efc237b380e
AyeshaNaime003 <ayeshashamim003@gmail.com>
2023-12-06 13:03:18 +0500

    prediction model created, registered, superuser created

--- base/models.py
+++ base/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Prediction(models.Model):
+    # Foreign Key relationship with the User model
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='predictions')
+
+    # Fields for predictions
+    GENDER_CHOICES = [ ('male', 'Male'),
+        ('female', 'Female'),]
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+
+    age = models.IntegerField()
+    height = models.FloatField()
+    weight = models.FloatField()
+
+    SMOKING_STATUS_CHOICES = [  ('never', 'Never Smoked'),
+        ('formerly', 'Formerly Smoked'),
+        ('smokes', 'Currently Smokes'),]
+    smoking_status = models.CharField(max_length=10, choices=SMOKING_STATUS_CHOICES)
+
+    RISK_CHOICES = [ (0, 'Low Risk'),
+        (1, 'High Risk'),]
+    risk = models.IntegerField(choices=RISK_CHOICES)
+
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Prediction for {self.user.username}'
+
+    class Meta:
+        ordering = ['-created']  # Order by creation time (latest first)
+
